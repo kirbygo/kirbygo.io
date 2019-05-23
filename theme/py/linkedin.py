d7b47d1b76c05b35cde0a2ad57acba85ce8a79da
Daniel Ruiz <kirbygo@hotmail.com>
2019-05-22 19:45:56 -0500

    Copia del pedo

--- theme/py/linkedin.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup as bs
-
- r = requests.get("https://www.linkedin.com/in/thomasbuhrmann")
- html = r.text
- 
- s = bs(html)
- 
-  s.find_all("span", class_='title')
